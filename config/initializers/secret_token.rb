651e81ca0b5f9cd37621a6b64f977cb74fe4c33e
Danny Yu <dannyyu92@gmail.com>
2014-02-26 15:33:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkoutApp::Application.config.secret_key_base = '4ff76406de2b8781f6e9f4a3df489e0a10dc51e12a121086daa9e449cfe9c88ec5273947f703d514a72a4c1543cda6e2ba76945acff1e25a1b66d637ded63327'
