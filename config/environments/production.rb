eaf35c462ffec21cf339904e0bbbadfade1e97e9
Danny Yu <dannyyu92@gmail.com>
2014-02-26 21:31:51 -0500

    Fix assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ WorkoutApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
