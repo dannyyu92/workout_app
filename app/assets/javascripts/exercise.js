acee601def817f46a8777b9588c32aa7f513aba7
Danny Yu <dannyyu92@gmail.com>
2014-03-04 18:55:37 -0500

    Tidy up jQuery

--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
   $('#exercise-text').keyup(function(event) {
     if (event.keyCode == 13) {
-      this.form.submit(".hidden");
-      return false;
+      this.form.submit();
     }
   })
 });
