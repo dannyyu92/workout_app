2834b80d5024c3161df2c5fe98ba1e7f19e04110
Danny Yu <dannyyu92@gmail.com>
2014-03-16 20:15:12 -0400

    Remove unnecessary code from scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ WorkoutApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  resources :exercises do
-    resources :ex_sets
-  end
+  resources :exercises
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
