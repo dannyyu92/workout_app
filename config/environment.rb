85c164bd6b707e58121656324ae3738c993a0610
Danny Yu <dannyyu92@gmail.com>
2014-03-25 15:36:41 -0400

    Fix incorrect Devise options in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 WorkoutApp::Application.initialize!
-config.action_mailer.default_url_options = { :host => 'localhost' }
